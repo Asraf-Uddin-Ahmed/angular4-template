9d8af27ebb212752abb6ce53c9cd8ff94248cbe6
Asraf Uddin Ahmed <13ratul@gmail.com>
2017-11-01 15:34:53 +0600

    dynamic-bootstrap-form creation feature add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { DynamicFormsCoreModule } from '@ng-dynamic-forms/core';
+import { DynamicFormsBootstrapUIModule } from '@ng-dynamic-forms/ui-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -8,6 +12,7 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { DynamicBootstrapFormComponent } from './components/dynamic-bootstrap-form/dynamic-bootstrap-form.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +20,14 @@ import { FooterComponent } from './components/footer/footer.component';
     LandingComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DynamicBootstrapFormComponent
   ],
   imports: [
     BrowserModule,
+    DynamicFormsCoreModule.forRoot(),
+    DynamicFormsBootstrapUIModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
