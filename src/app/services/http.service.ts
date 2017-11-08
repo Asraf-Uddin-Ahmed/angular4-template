910c8ae7f2565e9eb94d6c56ebfe4174a379d52f
Asraf Uddin Ahmed <13ratul@gmail.com>
2017-11-08 19:46:18 +0600

    POST, PUT and DELETE request add to http.service

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,12 +1,31 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
 
 @Injectable()
 export class HttpService {
 
-  constructor(private http: Http) { }
+  private readonly requestOptions = new RequestOptions();
+
+  constructor(private http: Http) {
+    const headers = new Headers();
+    // headers.append('Authorization', btoa('username:password'));
+    this.requestOptions.headers = headers;
+  }
 
   get(url) {
-    return this.http.get(url).map(res => res.json());
+    return this.http.get(url, this.requestOptions).map(res => res.json());
   }
+
+  post(url, data) {
+    return this.http.post(url, data, this.requestOptions).map(res => res.json());
+  }
+
+  put(url, data) {
+    return this.http.put(url, data, this.requestOptions).map(res => res.json());
+  }
+
+  delete(url) {
+    return this.http.delete(url, this.requestOptions).map(res => res.json());
+  }
+
 }
